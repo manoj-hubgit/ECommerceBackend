f731d764fbfc02823129b05b7786e618c062bae9
manoj-hubgit <manojannadurai2265@gmail.com>
2025-02-23 14:02:15 +0530

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app=express();
 app.use(express.json({ limit: '10mb' }));
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
